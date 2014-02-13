f0e5fcbc56365d72c4f0d8f03db9b32421d06a9e
Miles Georgi <azimux@gmail.com>
2014-02-13 09:34:36 -0800

    Remove annoying console write.

--- dist/cartodb.uncompressed.js
+++ dist/cartodb.uncompressed.js
@@ -21007,11 +21007,13 @@ if(!window.JSON) {
     //IE7 love
     if(typeof console !== "undefined") {
         _console = console;
+/*
         try {
           _console.log.apply(_console, ['cartodb.js ' + cartodb.VERSION])
         } catch(e) {
           _console = new _fake_console();
         }
+*/
     } else {
         _console = new _fake_console();
     }
